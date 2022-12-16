31c46709b487fed652d22fa2b90a081380f068ef
andreabertazzi <76404266+andreabertazzi@users.noreply.github.com>
2022-12-16 10:37:50 +0100

    Update particles_experiments.jl

--- particles_experiments.jl
+++ particles_experiments.jl
@@ -3,14 +3,14 @@ using Plots
 include("helper_split.jl")
 include("moves_samplers.jl")
 include("algorithms.jl")
-include("imaging_utils.jl")
+include("functions_particles.jl")
 
+N = 100 # number of particles
+iter = 10^4 # number of iterations per thinning / number of iterations
+num_thin = 1 * 10^5 # number of thinned samples
+δ = 1e-4
 
-N = 5 # number of particles
-iter = 10^6 # number of iterations
-δ = 1e-3
-
-a = 10.0
+a = .01
 V(r) = (1/r^12) - (1/r^6)
 W(r) = a * sqrt(1 + r^2)
 
@@ -25,28 +25,41 @@ Vrates(x,v,i)   = define_Vrates(Vprime, x, v, i, N)
 Wrates(x,v,i,j) = define_Wrates(Wprime, x, v, i, j, N)
 
 l = 1
-x_init = (collect(1:N).-N/2)./l
+b = 1
+nr_top = convert(Int,ceil(N/2))
+# x_init = zeros(N)
+# x_init[1:nr_top] = b .+ collect(1:nr_top)/l
+# x_init[nr_top+1:N] = -b .- collect((nr_top+1):N)/l
+# sort!(x_init)  # important step as else the LJ potential does not work well
+# x_init = (collect(1:N).-N/2)./l
+x_init = randn(N)/l
+sort!(x_init)
 v_init = rand((-1,1),N)
 chain_ZZS = splitting_zzs_particles(Vrates,Wrates,a,δ,N,iter,x_init,v_init);
-p = plot()
-# mu = zeros(iter+1)
+# chain_ZZS = thinned_splitting_zzs_particles(Vrates,Wrates,a,δ,N,iter,num_thin,x_init,v_init);
+
+
 pos = getPosition(chain_ZZS; want_array=true)
 mu = mean(pos; dims = 1)
-trace_potential = [interaction_pot(pos[:,i]) for i=1:iter+1]
-display(plot(trace_potential, label="Trace potential"))
-display(plot(mu, label="baricentre"))
+# plot(mu', label="baricentre")
 
 p = plot()
-for j = 1:N
-    positions = [chain_ZZS[i].position[j] for i = 1:(iter+1)];
-    # p = plot!(positions)
+for j = 1:100
+    positions = [chain_ZZS[i].position[j] for i = 1:length(chain_ZZS)];
+    # p = plot!(positions, legend=:no)
     c = positions - mu'
-    p = plot!(c)
+    p = plot!(c, legend=:no, title="Trajectories subtracting the baricentre")
     # pos[j,:] -= mu'
 end
-# mu = mu./N
-display(p)
 
+display(p)
+plot(mu', label="baricentre")
+trace_potential = [interaction_pot(pos[:,i]) for i=1:length(chain_ZZS)]
+display(plot(trace_potential, label="Trace potential", yaxis=:log))
+
+
+
+# savefig(p, string("trajectories.pdf"))
 
 # positions1 = [chain_ZZS[i].position[1] for i = 1:iter];
 # positions2 = [chain_ZZS[i].position[2] for i = 1:iter];
